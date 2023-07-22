Fixed masking

src/Model.py
@@ -230,7 +230,7 @@ class Model():
                     
                 # Forward pass to get the predicted unstylized audio
                 # from the interpolated audio
-                stylized_pred = self.model(audio_super, conditional if conditional_audio is not None else None, positional_embeddings, masks_stylized)
+                stylized_pred = self.model(audio_super, conditional if conditional_audio is not None else None, positional_embeddings, masks_stylized, masks_conditional)
                 
                 # Compute loss
                 # We want the model to predict the stylized audio
@@ -241,7 +241,7 @@ class Model():
                 loss.backward()
                 optimizer.step()
                 
-                # print(f"Epoch: {epoch} | Loss: {loss.item()}")
+                print(f"Epoch: {epoch} | Loss: {loss.item()}")
                 
                 batch_loss += loss.item()
                 
@@ -255,24 +255,30 @@ class Model():
             ## Audio samples
             if not os.path.exists("audio_samples/epoch_{}".format(epoch)):
                 os.makedirs("audio_samples/epoch_{}".format(epoch))
+            
+            # Remvoe zero pad from audio
+            unstylized = unstylized[:1, :, :masks_stylized[0].sum()]
+            if not self.use_noise:
+                conditional = conditional[:1, :, :masks_conditional[0].sum()]
+                
             # Generate audio prediction by diffusing the unstylized audio to the predicted stylized audio
-            stylized_audio_pred = self.diffusion_utils.sample_data(self.model, unstylized[:1, :, :masks_stylized[0].sum()], cond=conditional[:1] if conditional_audio is not None else None)
+            stylized_audio_pred = self.diffusion_utils.sample_data(self.model, unstylized, cond=conditional if conditional_audio is not None else None)
             stylized_audio_pred *= self.scale
             # Save audio samples
-            torchaudio.save(f"audio_samples/epoch_{epoch}/stylized.wav", stylized_audio[0].cpu() * self.scale, 24000)
+            torchaudio.save(f"audio_samples/epoch_{epoch}/stylized.wav", stylized_audio[0].cpu(), 24000)
             if not self.use_noise:
-                torchaudio.save(f"audio_samples/epoch_{epoch}/unstylized.wav", unstylized_audio[0].cpu() * self.scale, 24000)
+                torchaudio.save(f"audio_samples/epoch_{epoch}/unstylized.wav", unstylized_audio[0].cpu(), 24000)
             torchaudio.save(f"audio_samples/epoch_{epoch}/unstylized_recon.wav", self.encodec_model.decoder(stylized_audio_pred.to(self.device))[0].cpu(), 24000)
             
             # Save model checkpoints
-            if not os.path.exists("checkpoints2/epoch_{}".format(epoch)):
-                os.makedirs("checkpoints2/epoch_{}".format(epoch))
-            torch.save(self.model.state_dict(), f"checkpoints2/epoch_{epoch}/model.pth")
+            if not os.path.exists("checkpoints3/epoch_{}".format(epoch)):
+                os.makedirs("checkpoints3/epoch_{}".format(epoch))
+            torch.save(self.model.state_dict(), f"checkpoints3/epoch_{epoch}/model.pth")
             
             # Save optimizer checkpoints
-            if not os.path.exists("checkpoints2/epoch_{}".format(epoch)):
-                os.makedirs("checkpoints2/epoch_{}".format(epoch))
-            torch.save(optimizer.state_dict(), f"checkpoints2/epoch_{epoch}/optimizer.pth")
+            if not os.path.exists("checkpoints3/epoch_{}".format(epoch)):
+                os.makedirs("checkpoints3/epoch_{}".format(epoch))
+            torch.save(optimizer.state_dict(), f"checkpoints3/epoch_{epoch}/optimizer.pth")
             
             
             

src/blocks/convolution/ConditionalBlock.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from ..transformer.PositionalEncoding import PositionalEncoding
 from .MultiHeadAttention import MultiHeadAttention
 from .wideResNet import ResnetBlock
+from .MaskedInstanceNorm1d import MaskedInstanceNorm1d
 
 
 
@@ -14,16 +15,18 @@ class ConditionalBlock(nn.Module):
         self.cond_dim = cond_dim
         self.embed_dim = embed_dim
         
-        self.attn1 = MultiHeadAttention(embed_dim, 8, query_dim=cond_dim, key_dim=embed_dim, value_dim=embed_dim, output_dim=embed_dim)
-        self.layer_norm1 = nn.GroupNorm(1, embed_dim)
+        self.attn1 = MultiHeadAttention(embed_dim, 8, query_dim=embed_dim, key_dim=cond_dim, value_dim=embed_dim, output_dim=embed_dim)
+        # self.layer_norm1 = nn.GroupNorm(1, embed_dim)
+        self.layer_norm1 = MaskedInstanceNorm1d(embed_dim)
         
         self.resBlock = ResnetBlock(embed_dim, embed_dim)
         
         self.attn2 = MultiHeadAttention(embed_dim, 8, query_dim=cond_dim, key_dim=embed_dim, value_dim=embed_dim, output_dim=embed_dim)
-        self.layer_norm2 = nn.GroupNorm(1, embed_dim)
+        # self.layer_norm2 = nn.GroupNorm(1, embed_dim)
+        self.layer_norm2 = MaskedInstanceNorm1d(embed_dim)
         
         
-    def forward(self, x, y=None, mask=None):
+    def forward(self, x, y=None, mask=None, mask_cond=None):
         # Identity if no conditional information
         if type(y) == type(None):
             return x
@@ -31,14 +34,17 @@ class ConditionalBlock(nn.Module):
         # Residual connection
         res = x.clone()
         
-        # First MHA, cross where the queries are the conditional information
-        # and the keys (and values) are the input
-        x = self.layer_norm1(self.attn1(y, x, x))
+        # First MHA, cross where the queries are the input sequence,
+        # the keys are the conditional information and the values are the input sequence
+        # Basically, the input sequence is conditioned on the conditional information
+        x = self.layer_norm1(self.attn1(x, y, x, mask, mask_cond, mask), mask_cond) * (mask_cond if type(mask_cond) == torch.Tensor else 1)
         
         # Resnet block to extract features from the input
-        x = self.resBlock(x, mask=mask)
+        x = self.resBlock(x, mask=mask_cond)
         
-        # Second MHA, cross where the keys are the conditional information,
-        # the queries are the input and the values are the output of the resnet block
-        return self.layer_norm2(self.attn2(y, res, x, transpose_scores=True) + res)
+        # Second MHA, cross where the queries are the conditional information,
+        # the keys are the input sequence and the values are the intermediate output
+        # Basically, the conditional information is conditioned on the input sequence
+        # and the intermediate output is transformed on this conditional matrix
+        return self.layer_norm2(self.attn2(y, res, x, mask_cond, mask, mask_cond) + res, mask) * (mask if type(mask) == torch.Tensor else 1)
         

src/blocks/convolution/MultiHeadAttention.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 
+try:
+    from src.blocks.convolution.WeightStandardizedConv1d import WeightStandardizedConv1d
+except ModuleNotFoundError:
+    from .WeightStandardizedConv1d import WeightStandardizedConv1d
+
 
 
 
@@ -15,10 +20,14 @@ class MultiHeadAttention(nn.Module):
         self.value_dim = value_dim if value_dim else embed_dim
         self.output_dim = output_dim if output_dim else embed_dim
         
-        self.q_proj = nn.Conv1d(self.query_dim, embed_dim, 1)
-        self.k_proj = nn.Conv1d(self.key_dim, embed_dim, 1)
-        self.v_proj = nn.Conv1d(self.value_dim, embed_dim, 1)
-        self.out_proj = nn.Conv1d(embed_dim, self.output_dim, 1)
+        # self.q_proj = nn.Conv1d(self.query_dim, embed_dim, 1)
+        # self.k_proj = nn.Conv1d(self.key_dim, embed_dim, 1)
+        # self.v_proj = nn.Conv1d(self.value_dim, embed_dim, 1)
+        # self.out_proj = nn.Conv1d(embed_dim, self.output_dim, 1)
+        self.q_proj = WeightStandardizedConv1d(self.query_dim, embed_dim, 1)
+        self.k_proj = WeightStandardizedConv1d(self.key_dim, embed_dim, 1)
+        self.v_proj = WeightStandardizedConv1d(self.value_dim, embed_dim, 1)
+        self.out_proj = WeightStandardizedConv1d(embed_dim, self.output_dim, 1)
         
         
     def _split_heads(self, x):
@@ -30,15 +39,19 @@ class MultiHeadAttention(nn.Module):
         return x.reshape(x.shape[0], self.embed_dim, -1)
         
         
-    def forward(self, q, k, v, transpose_scores=False):
+    def forward(self, q, k, v, query_mask=None, key_mask=None, value_mask=None, transpose_scores=False):
         # Project the queries, keys and values
-        q, k, v = self.q_proj(q), self.k_proj(k), self.v_proj(v)
+        q, k, v = self.q_proj(q, query_mask, norm=False), self.k_proj(k, key_mask, norm=False), self.v_proj(v, value_mask, norm=False)
         
         # Split each embedding into self.num_heads pieces
         q, k, v = self._split_heads(q), self._split_heads(k), self._split_heads(v)
         
-        # Compute attention like normal
-        scores = ((k.permute(0, 1, 3, 2)@q) / (self.embed_dim ** 0.5)).softmax(dim=-1)
+        # Compute attention like normal (along time dimension)
+        scores = ((q.permute(0, 1, 3, 2)@k) 
+                    * (query_mask.unsqueeze(-1) if type(query_mask) == torch.Tensor else 1)
+                    * (key_mask.unsqueeze(-2) if type(key_mask) == torch.Tensor else 1)
+                  / (self.embed_dim ** 0.5)).softmax(dim=-1) \
+                      * (key_mask.unsqueeze(-2) if type(key_mask) == torch.Tensor else 1)
         
         # Compute the output
         if transpose_scores:
@@ -50,5 +63,5 @@ class MultiHeadAttention(nn.Module):
         out = self._combine_heads(out)
         
         # Project output and return
-        return self.out_proj(out)
+        return self.out_proj(out, key_mask if not transpose_scores else query_mask, norm=False)
         

src/blocks/convolution/unetBlock.py
@@ -61,9 +61,10 @@ class unetBlock(nn.Module):
     #   y - (optional) Tensor of shape (N, cond_dim, T)
     #   t - (optional) Tensor of shape (N, t_dim)
     #   mask - (optional) Tensor of shape (N, 1, T)
+    #   mask_cond - (optional) Tensor of shape (N, 1, T)
     # Output:
     #   Tensor of shape (N, outCh, L, W)
-    def forward(self, X, y=None, t=None, mask=None):
+    def forward(self, X, y=None, t=None, mask=None, mask_cond=None):
         # Class assertion
         if y != None:
             assert self.useCls == True, \
@@ -73,7 +74,7 @@ class unetBlock(nn.Module):
             if type(b) == ResnetBlock:
                 X = b(X, t, mask)
             elif type(b) == ConditionalBlock or type(b) == ConditionalBlock2:
-                X = b(X, y)
+                X = b(X, y, mask, mask_cond)
             else:
                 X = b(X)
         return X

src/blocks/convolution/wideResNet.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
-from einops import rearrange, reduce
-from functools import partial
-import torch.nn.functional as F
+from einops import rearrange
 from src.blocks.convolution.MaskedInstanceNorm1d import MaskedInstanceNorm1d
+
+try:
+    from src.blocks.convolution.WeightStandardizedConv1d import WeightStandardizedConv1d
+except ModuleNotFoundError:
+    from .WeightStandardizedConv1d import WeightStandardizedConv1d
 
 
 
@@ -15,36 +18,6 @@ from src.blocks.convolution.MaskedInstanceNorm1d import MaskedInstanceNorm1d
 # Check if something exists. Return None if it doesn't
 def exists(x):
     return x is not None
-
-
-
-
-
-# Weight standardization is shown to improve convolutions
-# when using groupNorm.
-class WeightStandardizedConv1d(nn.Conv1d):
-    """
-    https://arxiv.org/abs/1903.10520
-    weight standardization purportedly works synergistically with group normalization
-    """
-
-    def forward(self, x, mask=None):
-        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
-
-        weight = self.weight
-        mean = reduce(weight, "o ... -> o 1 1", "mean")
-        var = reduce(weight, "o ... -> o 1 1", partial(torch.var, unbiased=False))
-        normalized_weight = (weight - mean) * (var + eps).rsqrt()
-
-        return F.conv1d(
-            x,
-            normalized_weight,
-            self.bias,
-            self.stride,
-            self.padding,
-            self.dilation,
-            self.groups,
-        ) * (mask if type(mask) == torch.Tensor else 1)
 
 
 

src/models/U_Net.py
@@ -10,11 +10,15 @@ try:
     from src.blocks.convolution.Efficient_Channel_Attention import Efficient_Channel_Attention
     from src.blocks.convolution.Multihead_Attn import Multihead_Attn
     from src.blocks.convolution.wideResNet import WeightStandardizedConv1d
+    from src.blocks.convolution.WeightStandardizedConv1d import WeightStandardizedConv1d
+    from src.blocks.convolution.WeightStandardizedConvTranspose1d import WeightStandardizedConvTranspose1d
 except ModuleNotFoundError:
     from ..blocks.convolution.unetBlock import unetBlock
     from ..blocks.convolution.Efficient_Channel_Attention import Efficient_Channel_Attention
     from ..blocks.convolution.Multihead_Attn import Multihead_Attn
-    from ..blocks.convolution.wideResNet import WeightStandardizedConv1d
+    from ..blocks.convoltuion.WeightStandardizedConv1d import WeightStandardizedConv1d
+    from ..blocks.convoltuion.WeightStandardizedConvTranspose1d import WeightStandardizedConvTranspose1d
+    
 
 
 
@@ -80,7 +84,7 @@ class U_Net(nn.Module):
                 blocks.append(unetBlock(embCh*(2**(chMult*i)), embCh*(2**(chMult*i)), blk_types, cond_dim, t_dim, dropoutRate=dropoutRate, atn_resolution=atn_resolution))
                 blocks.append(unetBlock(embCh*(2**(chMult*i)), outCh, blk_types, cond_dim, t_dim, dropoutRate=dropoutRate, atn_resolution=atn_resolution))
             else:
-                blocks.append(nn.ConvTranspose1d(embCh*(2**(chMult*(i))), embCh*(2**(chMult*(i))), kernel_size=4, stride=2, padding=1))
+                blocks.append(WeightStandardizedConvTranspose1d(embCh*(2**(chMult*(i))), embCh*(2**(chMult*(i))), kernel_size=4, stride=2, padding=1))
                 blocks.append(unetBlock(2*embCh*(2**(chMult*i)), embCh*(2**(chMult*(i-1))), blk_types, cond_dim, t_dim, dropoutRate=dropoutRate, atn_resolution=atn_resolution))
         self.upBlocks = nn.Sequential(
             *blocks
@@ -110,7 +114,9 @@ class U_Net(nn.Module):
     #       X value of shape (N, t_dim)
     #   masks - (optional) Batch of masks for each X value
     #       of shape (N, 1, T)
-    def forward(self, X, y=None, t=None, masks=None):
+    #   masks_cond - (optional) Batch of masks for each c value
+    #       of shape (N, 1, T2)
+    def forward(self, X, y=None, t=None, masks=None, masks_cond=None):
         # conditional information assertion
         if type(y) != type(None):
             assert type(self.cond_dim) != type(None), "cond_dim must be specified when using condtional information."
@@ -123,7 +129,7 @@ class U_Net(nn.Module):
         residuals = []
         residual_masks = []
 
-        X = self.inConv(X, masks)
+        X = self.inConv(X, masks, norm=False)
         
         # Send the input through the downsampling blocks
         # while saving the output of each one
@@ -131,7 +137,7 @@ class U_Net(nn.Module):
         b = 0
         while b < len(self.downBlocks):
             # Convoltuion blocks
-            X = self.downBlocks[b](X, y, t, masks)
+            X = self.downBlocks[b](X, y, t, masks, masks_cond)
             
             # Save residual from convolutions
             residuals.append(X.clone())
@@ -144,7 +150,7 @@ class U_Net(nn.Module):
                 masks = masks[:, :, ::2] if type(masks) == torch.Tensor else None
                 
                 # Downsample the input
-                X = self.downBlocks[b](X, masks)
+                X = self.downBlocks[b](X, masks, norm=False)
                 b += 1
             
         # Reverse the residuals
@@ -163,24 +169,24 @@ class U_Net(nn.Module):
         b = 0
         while b < len(self.upBlocks):
             # Upsample the input and get he masks
-            if b < len(self.upBlocks) and type(self.upBlocks[b]) == nn.ConvTranspose1d:
-                # Upsample
-                X = self.upBlocks[b](X)
-                b += 1
-                
+            if b < len(self.upBlocks) and type(self.upBlocks[b]) == WeightStandardizedConvTranspose1d:
                 # Get masks for this layer
                 masks = residual_masks.pop()
                 
+                # Upsample
+                X = self.upBlocks[b](X, masks, norm=False)
+                b += 1
+                
             # Other residual blocks
             if len(residuals) > 0:
-                X = self.upBlocks[b](torch.cat((X[:, :, :residuals[0].shape[-1]], residuals[0]), dim=1), y, t, masks)
+                X = self.upBlocks[b](torch.cat((X[:, :, :residuals[0].shape[-1]], residuals[0]), dim=1), y, t, masks, masks_cond)
                 
             # Final residual block
             else:
-                X = self.upBlocks[b](X, y, t, masks)
+                X = self.upBlocks[b](X, y, t, masks, masks_cond)
             b += 1
             residuals = residuals[1:]
         
         # Send the output through the final block
         # and return the output
-        return self.out(X, masks)
+        return self.out(X, masks, norm=False)

src/train.py
@@ -7,11 +7,13 @@ from src.Model import Model
 from TTS.api import TTS
 import sys
 import random
+import contextlib
 
 
 
 
 class WavDataset(Dataset):
+    @torch.no_grad()
     def __init__(self, root_dir, load_in_memory=False, limit=-1):
         self.load_in_memory = load_in_memory
         
@@ -68,11 +70,11 @@ class WavDataset(Dataset):
         if self.load_in_memory:
             self.data = [(waveform, text, self.tts.tts(text)) for waveform, text in self.data]
         
-
+    @torch.no_grad()
     def __len__(self):
         return self.total
     
-    
+    @torch.no_grad()
     def load_item(self, idx, only_wav=False):
         # Get the speaker and index in speaker
         speaker, idx = self.reverse_idx[idx]
@@ -101,14 +103,8 @@ class WavDataset(Dataset):
             text = ''.join([c for c in text if c.isalnum() or c == ' ']) + '.'
             
 
-            # Turn off print dubugging
-            old_stdout = sys.stdout
-            sys.stdout = open(os.devnull, "w")
-            
-            waveform_unstylized, sample_rate_unstylized = (torch.tensor(self.tts.tts(text)).float().unsqueeze(0), self.tts_sr)
-            
-            # Turn back on printing
-            sys.stdout = old_stdout
+            with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+                waveform_unstylized, sample_rate_unstylized = (torch.tensor(self.tts.tts(text)).float().unsqueeze(0), self.tts_sr)
         
         # Resample audio to 24000 Hz
         if sample_rate != 24000:
@@ -118,6 +114,7 @@ class WavDataset(Dataset):
         return waveform, waveform_unstylized, text
     
 
+    @torch.no_grad()
     def __getitem__(self, idx):
         # Load in the data
         waveform, waveform_unstylized, text = self.load_item(idx)
@@ -136,7 +133,7 @@ class WavDataset(Dataset):
             
             # Get the conditional audio
             for i in range(number_of_indices):
-                # Get the new index and return it so it can't be used again
+                # Get the new index and remove it so it can't be used again
                 idx2 = random.choice(indices)
                 indices.remove(idx2)
                 
@@ -170,14 +167,15 @@ class WavDataset(Dataset):
 
 def train():
     data_path = "audio_stylized_speaker"
-    batch_size = 20
+    batch_size = 16
     num_workers = 8
     prefetch_factor = 3
-    limit = 10
+    limit = 25
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     use_noise = False
     
-    checkpoint_path = "checkpoints2/epoch_3/"
+    # checkpoint_path = "checkpoints2/epoch_30/"
+    checkpoint_path = None
     
     
     
@@ -191,9 +189,9 @@ def train():
                             batch_size=batch_size,
                             shuffle=True,  
                             collate_fn=lambda x: x,
-                            num_workers=num_workers,
-                            prefetch_factor=prefetch_factor,
-                            persistent_workers=True
+                            num_workers=num_workers if num_workers > 0 else 0,
+                            prefetch_factor=prefetch_factor if num_workers > 0 else None,
+                            persistent_workers=True if num_workers > 0 else False
     )
     
     
Fixed Loss Calculation

src/Model.py
@@ -302,7 +302,11 @@ class Model(nn.Module):
                 
                 # Compute loss
                 # We want the model to predict the stylized audio
-                loss = loss_fn(stylized, stylized_pred)
+                # Note that we also need to account for the masked terms so
+                # the average doesn't get thrown off. With the mask, the
+                # average will be a lot lower for shorter sequences.
+                # loss = loss_fn(stylized, stylized_pred)
+                loss = (((stylized-stylized_pred)**2).flatten(1, -1).sum(1)/(masks_stylized.squeeze(1).sum(1)*stylized.shape[1])).mean()
                 
                 # Backward pass
                 loss.backward()
@@ -329,6 +333,8 @@ class Model(nn.Module):
                 # Save audio samples
                 if num_steps % save_every_steps == 0:
                     with torch.no_grad():
+                        print(f"Step: {num_steps} | Loss: {batch_loss / num_steps}")
+                        
                         ## Audio samples
                         if not os.path.exists(f"{sample_dir}/step_{num_steps}"):
                             os.makedirs(f"{sample_dir}/step_{num_steps}")
@@ -395,21 +401,25 @@ class Model(nn.Module):
             unstylized = torch.tensor(self.tts.tts(text)).float()
         except RuntimeError:
             unstylized = torch.tensor(self.tts.tts(text + "...")).float()
+            
+        # Resample generated audio to 24000 Hz
+        unstylized = torchaudio.transforms.Resample(22050, 24000)(unstylized)
         
         # Load in the conditional audio
         conditionals = [torchaudio.load(path) for path in conditionals]
-        conditionals = [torchaudio.transforms.Resample(c[1], 24000)(c[0]) for c in conditionals]
+        conditionals = torch.cat([torchaudio.transforms.Resample(c[1], 24000)(c[0]) for c in conditionals], dim=1)
         
         # Pass the data through the encodec
         unstylized, _ = self.process_data(unstylized.unsqueeze(0))
-        conditionals = [self.process_data(c)[0] for c in conditionals]
+        # conditionals = [self.process_data(c)[0] for c in conditionals]
+        conditionals, _ = self.process_data(conditionals)
         
         # Pad the unstylized audio to the nearest second
         # Note: 75 is a second in the latent dimension
-        unstylized = torch.nn.functional.pad(unstylized, (0, 75 - unstylized.shape[2]%75, 0, 0))
+        # unstylized = torch.nn.functional.pad(unstylized, (0, 75 - unstylized.shape[2]%75, 0, 0))
         
         # Concatenate the conditional audio along the time dimension
-        conditionals = torch.cat(conditionals, dim=2)
+        # conditionals = torch.cat(conditionals, dim=2)
         
         # Permute audio to be of shape (N, E, T)
         unstylized = unstylized.to(self.device) / self.scale

src/infer.py
@@ -6,9 +6,9 @@ from src.Model import Model
 
 
 def infer():
-    checkpoint_path = "checkpoints_cond2_new_new/step_1000/"
-    text = "I agree with you, but I hate you!"
-    condition_paths = ["audio_stylized_speaker/29/1.wav", "audio_stylized_speaker/29/2.wav"]
+    checkpoint_path = "checkpoints_cond2_new_new/step_14000/"
+    text = 'I wanted to know more about the subject, so I asked about it.'
+    condition_paths = ["audio_stylized_speaker/45/1.wav", "audio_stylized_speaker/45/2.wav"]
     num_steps = 100
     outfile = "output.wav"
     device = torch.device("cuda:0")

src/train.py
@@ -40,12 +40,12 @@ class WavDataset(Dataset):
                     
                     if not speaker in self.speaker_map:
                         self.speaker_map[speaker] = []
-                        
-                    # Speaker to index in data
-                    self.speaker_map[speaker].append(self.total)
                     
                     # Data index to speaker and index in speaker
                     self.reverse_idx.append((speaker, len(self.speaker_map[speaker])))
+                    
+                    # Speaker to index in data
+                    self.speaker_map[speaker].append(self.total)
                     
                     # Add data to list
                     self.data.append((
@@ -61,9 +61,11 @@ class WavDataset(Dataset):
             
             
         # Load in the TTS model
-        model_name = 'tts_models/en/ljspeech/speedy-speech'
-        self.tts_sr = 22050
+        # model_name = 'tts_models/en/ljspeech/speedy-speech'
+        # self.tts_sr = 22050
+        model_name = 'tts_models/en/ljspeech/glow-tts'
         self.tts = TTS(model_name, gpu=False)
+        self.tts_sr = self.tts.synthesizer.output_sample_rate
         
         
         # Transcribe the text using TTS
@@ -76,14 +78,13 @@ class WavDataset(Dataset):
     
     @torch.no_grad()
     def load_item(self, idx, only_wav=False):
-        # Get the speaker and index in speaker
-        speaker, idx = self.reverse_idx[idx]
-        
         # Get the data entry
         data = self.data[idx]
         
         # Get the waveform and sample rate from memory
         if self.load_in_memory:
+            raise NotImplementedError
+            
             waveform, sample_rate = data[0]
             
             if only_wav:
@@ -93,6 +94,10 @@ class WavDataset(Dataset):
             waveform_unstylized, sample_rate_unstylized = (data[2], self.tts_sr)
         else:
             waveform, sample_rate = torchaudio.load(data[0])
+            
+            # Resample
+            if sample_rate != 24000:
+                waveform = torchaudio.transforms.Resample(sample_rate, 24000)(waveform)
             
             if only_wav:
                 return waveform
@@ -107,8 +112,6 @@ class WavDataset(Dataset):
                 waveform_unstylized, sample_rate_unstylized = (torch.tensor(self.tts.tts(text)).float().unsqueeze(0), self.tts_sr)
         
         # Resample audio to 24000 Hz
-        if sample_rate != 24000:
-            waveform = torchaudio.transforms.Resample(sample_rate, 24000)(waveform)
         waveform_unstylized = torchaudio.transforms.Resample(sample_rate_unstylized, 24000)(waveform_unstylized)
         
         return waveform, waveform_unstylized, text
@@ -120,7 +123,7 @@ class WavDataset(Dataset):
         waveform, waveform_unstylized, text = self.load_item(idx)
         
         # Get the speaker and index in speaker
-        speaker, _ = self.reverse_idx[idx]
+        speaker, speaker_idx = self.reverse_idx[idx]
         
         # Get another random data entry from the same speaker, but
         # with a different index value.
@@ -169,9 +172,9 @@ def collate_fn(batch):
 def train():
     # Data params
     data_path = "audio_stylized_speaker"
-    num_workers = 8
-    prefetch_factor = 3
-    limit = 50
+    num_workers = 7#8
+    prefetch_factor = 4
+    limit = 200
     
     # Model params
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -191,9 +194,11 @@ def train():
     use_scheduler = True
     sample_dir = "audio_samples_cond2_new_new"
     checkpoints_dir = "checkpoints_cond2_new_new"
+    # sample_dir = "del_"
+    # checkpoints_dir = "del_"
     
     # Loading params
-    # pretrained_checkpoint_path = "checkpoints_cond2_new_new/epoch_0/"
+    # pretrained_checkpoint_path = "checkpoints_cond2_new_new/step_11000/"
     pretrained_checkpoint_path = None
     
     
@@ -211,7 +216,7 @@ def train():
                             collate_fn=collate_fn,
                             num_workers=num_workers if num_workers > 0 else 0,
                             prefetch_factor=prefetch_factor if num_workers > 0 else None,
-                            persistent_workers=True,
+                            persistent_workers=True if num_workers > 0 else False,
                             # pin_memory=True,
     )
     

src/utils/Diffusion_Utils.py
@@ -109,17 +109,17 @@ class Diffusion_Utils:
             # Take Cold Diffusion step on the predicted x_0 posterior
             x_t = self.take_cold_diffusion_step(x_t, x_0_pred, t, t_next)
             
-        ### Final prediction
-        step = num_steps
+        # ### Final prediction
+        # step = num_steps
         
-        # Convert timestep between 0 and 1
-        # and start at 1 instead of 0. Then
-        # get the next timestep.
-        step = torch.tensor(step).to(x_1.device).repeat(x_1.shape[0], 1, 1)
-        t = 1-(step/num_steps)
+        # # Convert timestep between 0 and 1
+        # # and start at 1 instead of 0. Then
+        # # get the next timestep.
+        # step = torch.tensor(step).to(x_1.device).repeat(x_1.shape[0], 1, 1)
+        # t = 1-(step/num_steps)
         
-        # Predict the original x_1 (prior)
-        positional_encodings = self.t_to_positional_embeddings(t.squeeze(1, -1))
-        x_0_pred = model(x_t, cond, positional_encodings)
+        # # Predict the original x_1 (prior)
+        # positional_encodings = self.t_to_positional_embeddings(t.squeeze(1, -1))
+        # x_0_pred = model(x_t, cond, positional_encodings)
         
         return x_0_pred
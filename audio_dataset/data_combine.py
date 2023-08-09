Fixed masking issues again

audio_dataset/data_combine.py
@@ -12,10 +12,11 @@ import torchaudio
 # Params
 resample_rate = 16000   # Resample audio rate
 max_time = 10           # Max time of a single audio clip in seconds
+min_time = 1            # Min time of a single audio clip in seconds
 
 
 # Directory to save all wavs to
-out_dir = "audio_stylized"
+out_dir = "audio_stylized_speaker"
 
 # Two directories. One for VCTK and the other for LibriTTS
 VCTK_dir = "VCTK-Corpus"
@@ -30,42 +31,76 @@ if not os.path.exists(out_dir):
 # Count of the number of files in the output directory
 num_files = 0
 
-# Count of the number of files in the output directory
-num_files = 0
+# # First, we parse through the VCTK directory.
+# for root, dirs, files in os.walk(VCTK_dir + "/wav48"):
+#     for file in files:
+#         if file.endswith(".wav"):
+#             # Load in the file
+#             audio, sr = torchaudio.load(os.path.join(root, file))
 
-# First, we parse through the VCTK directory.
-for root, dirs, files in os.walk(VCTK_dir + "/wav48"):
-    for file in files:
-        if file.endswith(".wav"):
-            # Load in the file
-            audio, sr = torchaudio.load(os.path.join(root, file))
+#             # Make audio mono
+#             audio = audio.mean(0, keepdim=True).to(torch.float32)
 
-            # Make audio mono
-            audio = audio.mean(0, keepdim=True).to(torch.float32)
+#             # Resample audio
+#             audio = torchaudio.transforms.Resample(sr, resample_rate)(audio)
 
-            # Resample audio
-            audio = torchaudio.transforms.Resample(sr, resample_rate)(audio)
+#             # If the audio is longer than max_time, we split it into multiple files
+#             if audio.shape[1] > max_time * resample_rate:
+#                 num_splits = int(audio.shape[1] / (max_time * resample_rate))
+#                 for i in range(num_splits):
+#                     # Save file if it's longer than a second
+#                     if audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)].shape[1] < resample_rate:
+#                         continue
 
-            # If the audio is longer than max_time, we split it into multiple files
-            if audio.shape[1] > max_time * resample_rate:
-                num_splits = int(audio.shape[1] / (max_time * resample_rate))
-                for i in range(num_splits):
-                    # Save file if it's longer than a second
-                    if audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)].shape[1] < resample_rate:
-                        continue
+#                     torchaudio.save(f"{out_dir}/{num_files}.wav", audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)], resample_rate)
+#                     num_files += 1
 
-                    torchaudio.save(f"{out_dir}/{num_files}.wav", audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)], resample_rate)
-                    num_files += 1
+#                 continue
 
-                continue
-
-            # Save the file to the output directory
-            torchaudio.save(f"{out_dir}/{num_files}.wav", audio, resample_rate)
-            num_files += 1
+#             # Save the file to the output directory
+#             torchaudio.save(f"{out_dir}/{num_files}.wav", audio, resample_rate)
+#             num_files += 1
             
 
-            if num_files % 1000 == 0:
-                print(f"{num_files} files processed")
+#             if num_files % 1000 == 0:
+#                 print(f"{num_files} files processed")
+
+
+
+# # Now, we parse through the LibriTTS directory.
+# for root, dirs, files in os.walk(LibriTTS_dir):
+#     for file in files:
+#         if file.endswith(".wav"):
+#             # Load in the file
+#             audio, sr = torchaudio.load(os.path.join(root, file))
+
+#             # Make audio mono
+#             audio = audio.mean(0, keepdim=True).to(torch.float32)
+
+#             # Resample audio
+#             audio = torchaudio.transforms.Resample(sr, resample_rate)(audio)
+
+#             # If the audio is longer than max_time, we split it into multiple files
+#             if audio.shape[1] > max_time * resample_rate:
+#                 num_splits = int(audio.shape[1] / (max_time * resample_rate))
+#                 for i in range(num_splits):
+#                     # Save file if it's longer than a second
+#                     if audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)].shape[1] < resample_rate:
+#                         continue
+
+#                     torchaudio.save(f"{out_dir}/{num_files}.wav", audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)], resample_rate)
+#                     num_files += 1
+
+#                 continue
+
+#             # Save the file to the output directory
+#             torchaudio.save(f"{out_dir}/{num_files}.wav", audio, resample_rate)
+#             num_files += 1
+            
+
+#             if num_files % 1000 == 0:
+#                 print(f"{num_files} files processed")
+
 
 
 
@@ -73,6 +108,24 @@ for root, dirs, files in os.walk(VCTK_dir + "/wav48"):
 for root, dirs, files in os.walk(LibriTTS_dir):
     for file in files:
         if file.endswith(".wav"):
+            # Get the speaker ID from the file name
+            speaker_id = file.split("_")[0]
+
+            # Make sure the speaker directory exists
+            if not os.path.exists(f"{out_dir}/{speaker_id}"):
+                os.makedirs(f"{out_dir}/{speaker_id}")
+
+            # Get the number of files in the speaker directory
+            file_num = len(os.listdir(f"{out_dir}/{speaker_id}"))//2
+            #if file_num > 10:
+            #    continue
+
+            # Load in the text file and save it
+            with open(os.path.join(root, file[:-4] + ".normalized.txt"), "r") as f:
+                text = f.read()
+                with open(f"{out_dir}/{speaker_id}/{file_num}.txt", "w", encoding="utf-8") as f_out:
+                    f_out.write(text.strip())
+
             # Load in the file
             audio, sr = torchaudio.load(os.path.join(root, file))
 
@@ -81,6 +134,10 @@ for root, dirs, files in os.walk(LibriTTS_dir):
 
             # Resample audio
             audio = torchaudio.transforms.Resample(sr, resample_rate)(audio)
+            
+            # Skip audio less than the min time
+            if audio.shape[1] < min_time*resample_rate:
+                continue
 
             # If the audio is longer than max_time, we split it into multiple files
             if audio.shape[1] > max_time * resample_rate:
@@ -90,15 +147,15 @@ for root, dirs, files in os.walk(LibriTTS_dir):
                     if audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)].shape[1] < resample_rate:
                         continue
 
-                    torchaudio.save(f"{out_dir}/{num_files}.wav", audio[:, i * (max_time * resample_rate) : (i + 1) * (max_time * resample_rate)], resample_rate)
+                    torchaudio.save(f"{out_dir}/{speaker_id}/{file_num}.wav", audio[:, i * (max_time * resample_rate) : (i + 1)>
                     num_files += 1
 
                 continue
 
             # Save the file to the output directory
-            torchaudio.save(f"{out_dir}/{num_files}.wav", audio, resample_rate)
+            torchaudio.save(f"{out_dir}/{speaker_id}/{file_num}.wav", audio, resample_rate)
             num_files += 1
-            
+
 
             if num_files % 1000 == 0:
                 print(f"{num_files} files processed")

src/Model.py
@@ -366,7 +366,6 @@ class Model(nn.Module):
                     
                     
                     
-                    
                 # Forward pass to get the predicted unstylized audio
                 # from the interpolated audio
                 stylized_pred = self.model(audio_super, conditional if conditional_audio is not None else None, positional_embeddings, text, masks_stylized, masks_conditional)
@@ -425,7 +424,7 @@ class Model(nn.Module):
                 # Save audio samples
                 if num_steps % save_every_steps == 0:
                     with torch.no_grad():
-                        print(f"Step: {num_steps} | Loss: {batch_loss / num_steps}")
+                        print(f"Step: {num_steps} | Loss: {batch_loss / batch_num}")
                         
                         ## Audio samples
                         if not os.path.exists(f"{sample_dir}/step_{num_steps}"):
@@ -488,14 +487,19 @@ class Model(nn.Module):
     # Given text and a list of conditionals, generate the stylized audio
     @torch.no_grad()
     def infer(self, text, conditionals=[], num_steps=100):
-        # Create the unstylized audio
-        try:
-            unstylized = torch.tensor(self.tts[0].tts(text)).float()
-        except RuntimeError:
-            unstylized = torch.tensor(self.tts[0].tts(text + "...")).float()
+        if not self.use_noise:
+            # Create the unstylized audio
+            try:
+                unstylized = torch.tensor(self.tts[0].tts(text)).float()
+            except RuntimeError:
+                unstylized = torch.tensor(self.tts[0].tts(text + "...")).float()
+                
+            # Resample generated audio to 24000 Hz
+            unstylized = torchaudio.transforms.Resample(22050, 24000)(unstylized)
             
-        # Resample generated audio to 24000 Hz
-        unstylized = torchaudio.transforms.Resample(22050, 24000)(unstylized)
+            unstylized, _ = self.process_data(unstylized.unsqueeze(0))
+        else:
+            unstylized = torch.randn(1, 128, 300)
         
         # Load in the conditional audio
         conditionals = [torchaudio.load(path) for path in conditionals]
@@ -505,7 +509,6 @@ class Model(nn.Module):
         text = self.CLIP[0].encode(text)
         
         # Pass the data through the encodec
-        unstylized, _ = self.process_data(unstylized.unsqueeze(0))
         # conditionals = [self.process_data(c)[0] for c in conditionals]
         conditionals, _ = self.process_data(conditionals)
         

src/blocks/convolution/ContextBlock.py
@@ -50,10 +50,22 @@ class ContextBlock(nn.Module):
         q, k, v = self._split_heads(q), self._split_heads(k), self._split_heads(v)
         
         # Compute attention along the embedding dimension
-        scores = ((q.transpose(-1, -2)@k) / (self.embed_dim//self.num_heads ** 0.5)).softmax(-1)
+        scores = ((k.transpose(-1, -2)@q) / (self.embed_dim//self.num_heads ** 0.5))
+        
+        # Mask scores along input (queries) dimension
+        if type(mask) == torch.Tensor:
+            scores = scores.masked_fill(~mask.unsqueeze(-2), -1e9)
+        
+        # Apply softmax
+        scores = scores.softmax(-1)
+        
+        # Apply mask along the context (keys) dimension
+        if type(mask_ctx) == torch.Tensor:
+            scores = scores.masked_fill(~mask_ctx.unsqueeze(-1), 0)
         
         # Compute the output
-        out = (scores@v.transpose(-1, -2)).transpose(-1, -2)
+        # out = (scores@v.transpose(-1, -2)).transpose(-1, -2)
+        out = v@scores
         
         # Compute the output and remove the heads
         out = self._combine_heads(out)

src/blocks/convolution/MultiHeadAttention.py
@@ -10,11 +10,12 @@ except ModuleNotFoundError:
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads, query_dim=None, key_dim=None, value_dim=None, output_dim=None):
+    def __init__(self, embed_dim, num_heads, norm_type="GN", query_dim=None, key_dim=None, value_dim=None, output_dim=None):
         super().__init__()
         
         self.embed_dim = embed_dim
         self.num_heads = num_heads
+        self.norm_type = norm_type
         self.query_dim = query_dim if query_dim else embed_dim
         self.key_dim = key_dim if key_dim else embed_dim
         self.value_dim = value_dim if value_dim else embed_dim
@@ -29,6 +30,20 @@ class MultiHeadAttention(nn.Module):
         self.v_proj = WeightStandardizedConv1d(self.value_dim, embed_dim, 1)
         self.out_proj = WeightStandardizedConv1d(embed_dim, self.output_dim, 1)
         
+        if norm_type == "pre_norm":
+            self.norm_q = nn.LayerNorm(embed_dim)
+            self.norm_k = nn.LayerNorm(embed_dim)
+            self.norm_v = nn.LayerNorm(embed_dim)
+        elif norm_type == "post_norm":
+            self.norm = nn.LayerNorm(embed_dim)
+        elif norm_type == "GN":
+            raise NotImplementedError("GN not implemented. Has issues with masks")
+            self.norm_q = nn.GroupNorm(num_heads, embed_dim)
+            self.norm_k = nn.GroupNorm(num_heads, embed_dim)
+            self.norm_v = nn.GroupNorm(num_heads, embed_dim)
+        else:
+            raise ValueError(f"norm_type must be 'pre_norm', 'post_norm', or 'GN', not {norm_type}")
+        
         
     def _split_heads(self, x):
         # Split the last dimension into (num_heads, depth)
@@ -39,10 +54,19 @@ class MultiHeadAttention(nn.Module):
         return x.reshape(x.shape[0], self.embed_dim, -1)
         
         
-    def forward(self, q, k, v, query_mask=None, key_mask=None, value_mask=None, transpose_scores=False):
+    def forward(self, q, k, v, query_mask=None, key_mask=None, value_mask=None, res=None, transpose_scores=False):
+        if self.norm_type == "pre_norm":
+            q = self.norm_q(q)
+            k = self.norm_k(k)
+            v = self.norm_v(v)
+        
         # Project the queries, keys and values
         # Note that the masked states are retained
         q, k, v = self.q_proj(q, query_mask), self.k_proj(k, key_mask), self.v_proj(v, value_mask)
+        
+        # Normalize each head with group norm
+        if self.norm_type == "GN":
+            q, k, v = self.norm_q(q), self.norm_k(k), self.norm_v(v)
         
         # Split each embedding into self.num_heads pieces
         q, k, v = self._split_heads(q), self._split_heads(k), self._split_heads(v)
@@ -83,5 +107,10 @@ class MultiHeadAttention(nn.Module):
         out = self._combine_heads(out)
         
         # Project output and return
-        return self.out_proj(out, query_mask if not transpose_scores else key_mask)
+        out = self.out_proj(out, query_mask if not transpose_scores else key_mask)
+        if type(res) == torch.Tensor:
+            out = out + res
+        if self.norm_type == "post_norm":
+            out = self.norm(out.transpose(-1, -2)).transpose(-1, -2)
+        return out
         

src/blocks/convolution/unetBlock.py
@@ -57,7 +57,7 @@ class unetBlock(nn.Module):
             elif blk == "cond3":
                 blocks.append(ContextBlock(curCh, cond_dim, name="cond"))
             elif blk == "atn":
-                blocks.append(MultiHeadAttention(curCh, 8))
+                blocks.append(MultiHeadAttention(curCh, 8, norm_type="post_norm"))
             elif blk == "ctx":
                 blocks.append(ContextBlock(curCh, c_dim, name="text_context"))
 
@@ -87,7 +87,7 @@ class unetBlock(nn.Module):
             elif type(b) == ConditionalBlock or type(b) == ConditionalBlock2 or (type(b) == ContextBlock and b.name=="cond"):
                 X = b(X, y, mask, mask_cond)
             elif type(b) == MultiHeadAttention:
-                X = b(X, X, X, mask, mask, mask)
+                X = b(X, X, X, mask, mask, mask, res=X)
                 # X = b(X.transpose(-1, -2), X.transpose(-1, -2), X.transpose(-1, -2))[0].transpose(-1, -2) 
             elif type(b) == ContextBlock:
                 X = b(X, context, mask, None)

src/infer.py
@@ -6,13 +6,13 @@ from src.Model import Model
 
 
 def infer():
-    checkpoint_path = "checkpoints_cond2_new_new/step_19000/"
+    checkpoint_path = "checkpoints/step_94000/"
     text = 'I wanted to know more about the subject, so I asked about it.'
     condition_paths = ["audio_stylized_speaker/6746/1.wav", "audio_stylized_speaker/6746/2.wav"]
     num_steps = 100
     outfile = "output.wav"
+    # device = torch.device("cuda:0")
     device = torch.device("cpu")
-    # device = torch.device("cpu")
     
     
     
@@ -22,9 +22,9 @@ def infer():
     cond_embed_dim = 128
     num_blocks = 3
     blk_types = [
-        ["res", "cond3", "ctx"],
-        ["res", "atn", "cond3", "ctx"],
-        ["res", "atn", "cond3", "ctx"],
+        ["res", "cond3", "ctx", "res"],
+        ["res", "atn", "cond3", "ctx", "res"],
+        ["res", "atn", "cond3", "ctx", "res"],
     ]
     use_noise = True
     noise_scheduler_type = "cosine"

src/models/U_Net.py
@@ -179,16 +179,13 @@ class U_Net(nn.Module):
                 b += 1
             
         # Reverse the residuals
-        residuals = residuals[::-1]
+        residuals = residuals[::-1] 
         
         # Send the output of the downsampling block
         # through the intermediate blocks
         # return X
         for b in self.intermediate:
-            try:
-                X = b(X, y=y, t=t, context=context, mask=masks, mask_cond=masks_cond)
-            except TypeError:
-                X = b(X)
+            X = b(X, y=y, t=t, context=context, mask=masks, mask_cond=masks_cond)
         
         # Send the intermediate batch through the upsampling
         # block to get the original shape

src/train.py
@@ -185,16 +185,16 @@ def train():
     
     # Model params
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cpu")
     embed_dim = 256
     t_embed_dim = 256
     cond_embed_dim = 128
     num_blocks = 3
     blk_types = [
-        ["res", "cond2", "ctx", "res"],
-        ["res", "atn", "cond2", "ctx", "res"],
-        ["res", "atn", "cond2", "ctx", "res"],
+        ["res", "cond3", "ctx", "res"],
+        ["res", "atn", "cond3", "ctx", "res"],
+        ["res", "atn", "cond3", "ctx", "res"],
     ]
-    # device = torch.device("cpu")
     use_noise = True
     noise_scheduler_type = "cosine"
     
@@ -203,15 +203,15 @@ def train():
     lr = 1e-4
     save_every_steps = 1000
     accumulation_steps = 2
-    use_scheduler = False
+    use_scheduler = True
     sample_dir = "audio_samples"
     checkpoints_dir = "checkpoints"
     # sample_dir = "del_"
     # checkpoints_dir = "del_"
     
     # Loading params
-    pretrained_checkpoint_path = "checkpoints/step_11000/"
-    # pretrained_checkpoint_path = None
+    # pretrained_checkpoint_path = "checkpoints/step_94000/"
+    pretrained_checkpoint_path = None
     
     
     

src/utils/Diffusion_Utils.py
@@ -73,10 +73,12 @@ class Diffusion_Utils:
     
     # Given data at timestep t and the predicted origin/prior, x_1,
     # take a DDIM step to get the next timestep
-    def take_ddim_step(self, x_t, x_1_pred, t_now, t_next, clamp=False):
+    def take_ddim_step(self, x_t, x_0_pred, t_now, t_next, clamp=False):
         # Compute the gamma values for the timesteps
         gammas = self.scheduler(t_now).reshape(-1, 1, 1)
         gammas_next = self.scheduler(t_next).reshape(-1, 1, 1)
+        
+        x_1_pred = (x_t - torch.sqrt(gammas)*x_0_pred)/torch.sqrt(1-gammas) # Obtained by solving the forward noising equation for x_1
         
         # DDIM without noise component
         if clamp:
@@ -128,10 +130,10 @@ class Diffusion_Utils:
             x_0_pred = model(x_t/1, cond, positional_encodings, context)*1
             
             # Take DDIM step on the predicted x_1 prior
-            # x_t = self.take_ddim_step(x_t, x_1_pred, t, t_next, step==0)
+            x_t = self.take_ddim_step(x_t, x_0_pred, t, t_next, torch.all(step==0))
             
             # Take Cold Diffusion step on the predicted x_0 posterior
-            x_t = self.take_cold_diffusion_step(x_t, x_0_pred, t, t_next)
+            # x_t = self.take_cold_diffusion_step(x_t, x_0_pred, t, t_next)
             
         # ### Final prediction
         # step = num_steps